b661eeb846773544da55c52d4089f54e95d1bdd2
Afsha10 <unityofbeing1@gmail.com>
2024-02-16 17:56:54 +0000

    Update config with AWS NODE_ENV

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,10 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000"
-    : "http://localhost:5000";
+export const baseUrl = (() => {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return "https://video-storage.onrender.com";
+    case "aws":
+      return "http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000";
+    default:
+      return "http://localhost:5000";
+  }
+})();
