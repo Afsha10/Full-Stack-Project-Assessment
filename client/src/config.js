fc713fa0abb2da9fb1e6478f2967284e5c242212
Afsha10 <unityofbeing1@gmail.com>
2024-02-19 22:22:21 +0000

    Use EC2 endpoint in production
    Make AWS production by using EC2 endpoint in production

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,8 @@
 export const baseUrl = (() => {
   switch (process.env.NODE_ENV) {
     case "production":
-      return "https://video-storage.onrender.com";
-    case "aws":
+      //   return "https://video-storage.onrender.com";
+      // case "aws":
       return "http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000";
     default:
       return "http://localhost:5000";
